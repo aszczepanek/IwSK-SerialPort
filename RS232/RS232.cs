2d8ad3a8616cfa51773ce0380f196b5d444d0d87
Aleksander Szczepanek <aleksander.szczepanek@gmail.com>
2013-05-18 16:13:24 +0200

    Main RS232 class.

---
+++ RS232/RS232.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO.Ports;
+
+namespace RS232
+{
+    /// <summary>
+    /// Główna klasa obsługująca port.
+    /// </summary>
+    class RS232
+    {
+        /// <summary>
+        /// Singleton.
+        /// </summary>
+        private RS232 _instance;
+
+        /// <summary>
+        /// Dostęp do singletonu.
+        /// </summary>
+        public RS232 Instance
+        {
+            get
+            {
+                if (_instance == null)
+                    _instance = new RS232();
+                return _instance;
+            }
+        }
+
+        /// <summary>
+        /// Pusty prywatny konstruktor - realizacja singletonu.
+        /// </summary>
+        private RS232() { }
+
+        /// <summary>
+        /// Port, który będzie używany do transmisji.
+        /// </summary>
+        private SerialPort _port;
+
+        
+
+        
+
+
+        /// <returns>Lista portów dostępnych w komputerze.</returns>
+        public static string[] GetSerialPorts()
+        {
+            return SerialPort.GetPortNames();
+        }
+    }
+}
